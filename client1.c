2a215d824672e3420fdf7be4f988a8a656162730
Bonnie <kwongb@flip1.engr.oregonstate.edu>
2017-05-04 21:16:46 -0700

    working on client

--- client1.c
+++ client1.c
@@ -3,7 +3,8 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <string.h>
-
+#include <unistd.h>
+#include <sys/socket.h>
 
 int main(int argc, char * argv[]){
   printf("hello\n");
@@ -13,15 +14,51 @@ int main(int argc, char * argv[]){
   struct hostent *server;
 
 
-  portno = 50050;
+  //portno = atoi(argv[2]);
+  portno = 4000; // 50050
 
-  sockfd = socket(AF_INET, SOCK_STREAM,0);
+  char buffer[256];
+
+  sockfd=socket(AF_INET, SOCK_STREAM,0);
+  bzero(&serv_addr, sizeof serv_addr);
 
   if(sockfd < 0){
-
    perror("Errro");
    exit(1);
   }
+
+
+  server = gethostbyname(argv[1]);
+  if(server == NULL){
+    fprintf(stderr, "no such host\n");
+    exit(1);
+  }
+
+  serv_addr.sin_family = AF_INET;
+  
+  serv_addr.sin_port = htons(portno);
+
+  inet_pton(AF_INET, "127.0.0.1", &(serv_addr.sin_addr));
+  
+  if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))< 0){
+   perror("error conecting\n");
+  } 
+  printf("Please enter message:");
+  bzero(buffer, 256);
+  fgets(buffer, 255, stdin);
+  n = write(sockfd, buffer, strlen(buffer));
+  
+  if(n < 0){
+    perror("error reading from socket\n");
+  }
+
+  printf("%s\n", buffer);
+  close(sockfd);
+ 
+
+
+
+
   return 0;
 
 }
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Usage:
+./client1 127.0.0.1 3030
+
+
+(or some other port)
