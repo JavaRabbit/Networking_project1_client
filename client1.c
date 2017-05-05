5a10b375675353941efd14db8b19857985b9481d
Bonnie <kwongb@flip2.engr.oregonstate.edu>
2017-05-05 00:08:42 -0700

    argv 1 works for flipx.engr.oregonstate.edu

--- client1.c
+++ client1.c
@@ -7,17 +7,31 @@
 #include <sys/socket.h>
 
 int main(int argc, char * argv[]){
-  printf("hello\n");
+
+  /*
+  // a string to get user's handle
+  char userHandle[11];
+  printf("hello, enter your name:\n");
+  fgets(userHandle, 11, stdin);
+
+  // trim off new line character
+  int newLineLocation = strlen(userHandle) - 1;
+  userHandle[newLineLocation] = '\0';
+
+  // say hi to user
+  printf("hi %s\n", userHandle);
+ */
 
   int sockfd, portno, n;
   struct sockaddr_in serv_addr;
-  struct hostent *server;
-
+  struct hostent *server, *gethostbyname();
+  struct in_addr ipv4addr;
 
   portno = atoi(argv[2]);
   // portno = 4300; // 50050
 
-  char buffer[256];
+  // spec: program should be able to send message up to 500 chars
+  char buffer[500];
 
   sockfd=socket(AF_INET, SOCK_STREAM,0);
   bzero(&serv_addr, sizeof serv_addr);
@@ -27,25 +41,34 @@ int main(int argc, char * argv[]){
    exit(1);
   }
 
-
+  
+  //server = gethostbyaddr(&ipv4addr, sizeof ipv4addr, AF_INET);
+   
   server = gethostbyname(argv[1]);
   if(server == NULL){
     fprintf(stderr, "no such host\n");
     exit(1);
   }
+  
+  // new line. see added gethostname hosttent
+  bcopy(server->h_addr, &(serv_addr.sin_addr.s_addr), server->h_length);
+  
 
   serv_addr.sin_family = AF_INET;
   
   serv_addr.sin_port = htons(portno);
+  //serv_addr.sin_addr.s_addr = inet_addr(argv[1]);  //  new line
 
-  inet_pton(AF_INET, "127.0.0.1", &(serv_addr.sin_addr));
+  // middle argument used to be "127.0.0.1"
+  inet_pton(AF_INET, server, &(serv_addr.sin_addr));
   
   if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))< 0){
-   perror("error conecting\n");
+   perror("error conecting cheshire\n");
   } 
+
   printf("Please enter message:");
-  bzero(buffer, 256);
-  fgets(buffer, 255, stdin);
+  bzero(buffer, 500);
+  fgets(buffer, 499, stdin);
   n = write(sockfd, buffer, strlen(buffer));
   
 
