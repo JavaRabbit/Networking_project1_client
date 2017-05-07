7922ca1897c0faacb35c652a43f7bd73231834de
Bonnie <kwongb@flip1.engr.oregonstate.edu>
2017-05-07 16:18:46 -0700

    first change after methodizing

--- client1.c
+++ client1.c
@@ -7,38 +7,54 @@
 #include <sys/socket.h>
 #include <stdbool.h>
 
+
+/*  method prototypes */
+void getUserHandle();
+void chat();
+void closeConnection();
+
+/*  variables */
+char userHandle[11];
+int sockfd;
+
+/* Method to get user handle */
+void getUserHandle(){
+  printf("Please enter your name:\n");
+  fgets(userHandle, 10, stdin);
+  
+  // trim off new line character
+  int newLineLocation = strlen(userHandle) - 1;
+  userHandle[newLineLocation] = '\0';
+ 
+  // gree the user
+  printf("Welcome to the chat client %s\n", userHandle);
+}
+
+
+
+
+
 int main(int argc, char * argv[]){
 
+  
   // verify the number of arguments to be 3
   if (argc !=3){
     printf("Usage example: ./client flip1.engr.oregonstate.edu <port number>\n");
     exit(1);
   }  
 
+  /*  Call method to get user handle */
+  getUserHandle();
 
-  // a string to get user's handle
-  char userHandle[11];
-  printf("hello, enter your name:\n");
-  fgets(userHandle, 10, stdin);
-
-  // trim off new line character
-  int newLineLocation = strlen(userHandle) - 1;
-  userHandle[newLineLocation] = '\0';
-
-  // say hi to user
-  printf("hi %s\n", userHandle);
- 
-
-  int sockfd, portno, n;
+  
+  int  portno;
   struct sockaddr_in serv_addr;
   struct hostent *server, *gethostbyname();
-  struct in_addr ipv4addr;
 
   portno = atoi(argv[2]);
-  // portno = 4300; // 50050
 
   // spec: program should be able to send message up to 500 chars
-  char buffer[500];
+  //char buffer[500];  this line moves to chat. remove
 
   sockfd=socket(AF_INET, SOCK_STREAM,0);
   bzero(&serv_addr, sizeof serv_addr);
@@ -49,7 +65,6 @@ int main(int argc, char * argv[]){
   }
 
   
-  //server = gethostbyaddr(&ipv4addr, sizeof ipv4addr, AF_INET);
    
   server = gethostbyname(argv[1]);
   if(server == NULL){
@@ -64,9 +79,7 @@ int main(int argc, char * argv[]){
   serv_addr.sin_family = AF_INET;
   
   serv_addr.sin_port = htons(portno);
-  //serv_addr.sin_addr.s_addr = inet_addr(argv[1]);  //  new line
 
-  // middle argument used to be "127.0.0.1"
   inet_pton(AF_INET, server, &(serv_addr.sin_addr));
   
   if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))< 0){
@@ -74,8 +87,16 @@ int main(int argc, char * argv[]){
    exit(1);
   } 
 
+  // call the chat method to start the chat
+  chat();
+  return 0;
 
+
+} // end of main method
+
+void chat(){
   char response[245];
+  char buffer[500];
 
   while(true){
     printf("Please enter message:");
@@ -93,8 +114,7 @@ int main(int argc, char * argv[]){
     }
 
 
-    n = write(sockfd, buffer, strlen(buffer));
-    printf("gets here %d\n");
+    int n = write(sockfd, buffer, strlen(buffer));
 
  
     recv(sockfd, response, 44,0);
@@ -116,6 +136,5 @@ int main(int argc, char * argv[]){
   }
   close(sockfd);
 
-  return 0;
 
 }
